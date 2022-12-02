dece73a580af30a907bc142fa6f6963b28d21707
jiachenl2021 <jiachenl@princeton.edu>
2022-12-02 14:26:09 -0500

    fixed: wrong class name in test_rank.py

--- tests/test_rank.py
+++ tests/test_rank.py
@@ -5,6 +5,6 @@ import rank
 pocket = [Card(value=10, suit='s'), Card(value=10, suit='h')]
 board = [Card(value=10, suit='c'), Card(value=10, suit='d'), Card(value=9, suit='d'), Card(value=8, suit='d'), Card(value=7, suit='d')]
 
-A = BestHand(pocket, board)
+A = rank.BestHand(pocket, board)
 print(A.besthand)
 print(A.rank)
