48ac6523ee74475196b635fd8b5cda0f2e543f69
hpres21 <paal.h.prestegaard.22@dartmouth.edu>
2022-12-02 17:37:18 -0500

    Update table.py code

--- table.py
+++ table.py
@@ -4,75 +4,106 @@ from dataclasses import dataclass
 @dataclass
 class Player:
     stack: int
-    position: int
     hand = []
-    is_big_blind = False
-    is_small_blind = False
-    is_dealer = False
+    bet_this_round: int = 0
+    current_decision = None
+    
+    def __repr__(self) -> str:
+        return str((self.stack, self.bet_this_round, self.current_decision, self.hand))
 
-    def draw_hand(self, deck):
+    def draw_hand(self, deck)-> None:
         self.hand = [deck.draw() for _ in range(2)]
 
-    def make_bet(self, table, bet):
-        if bet <= self.stack and bet > 0:
-            table.pot_size += bet
-            self.stack -= bet
-
-    def clear_hand(self):
+    def clear_hand(self)-> None:
         self.hand = []
 
-    def set_big_blind(self):
-        self.is_big_blind = True
-        self.is_small_blind = False
-        self.is_dealer = False
-
-    def set_small_blind(self):
-        self.is_small_blind = True
-        self.is_big_blind = False
-        self.is_dealer = False
-
-    def set_dealer(self):
-        self.is_dealer = True
-        self.is_big_blind = False
-        self.is_small_blind= False
-
-    def clear_status(self):
-        self.is_big_blind = False
-        self.is_small_blind = False
-        self.is_dealer = False
+    def decision(self, table):
+        action = input(f"You have {self.stack}, the current bet is {table.current_bet}. Please make a decision: ")
+        try:
+            action = int(action)
+            if action >= 2*table.current_bet and action <= self.stack:
+                self.current_decision = action - self.bet_this_round
+                self.bet_this_round += self.current_decision
+            else: 
+                print("Please bet a valid amount")
+                self.decision(table)
+        except ValueError:
+            if action.upper() == "CALL":
+                if table.current_bet != self.bet_this_round:
+                    self.current_decision = table.current_bet
+                    self.bet_this_round += self.current_decision
+                else:
+                    print("You cannot Call")
+                    self.decision(table)
+            elif action.upper() == "CHECK":
+                if table.current_bet == self.bet_this_round:
+                    self.current_decision = 0
+                else:
+                    print("You cannot Check")
+                    self.decision(table)
+            elif action.upper() == "FOLD":
+                self.current_decision = "FOLD"
+                self.bet_this_round = 0
+                self.current_decision = None
+            else:
+                print("Invalid decision.")
+                self.decision(table)
 
 
 
 
 @dataclass
-class Table:
+class Poker_Table:
     """
     Table class stores the information about the cards on the board, stack size, and players in the game.
     """
     max_num_players: int = 6
     big_blind: int = 2
-    pot_size = 0
+    pot_size:int = 0
     players = []
     board = []
+    current_bet: int  = 0
 
-    def __repr__(self):
-        return str(self.board)
+    def __repr__(self)-> str:
+        """
+        Not final form of function just for testing right now
+        """
+        return str((self.pot_size, self.current_bet, self.board, self.players))
 
-    def add_player(self, player):
+    def add_player(self, player)-> None:
         if len(self.players) <= self.max_num_players:
             self.players.append(player)
         else:
             print("Too many players. Wait for one to leave before you buy in.")
 
-    def remove_player(self, player):
+    def remove_player(self, player)-> None:
         self.players.pop(self.players.index(player))
         print(f"Player {player} removed from game with ${player.stack}")
 
-    def flop(self, deck):
+    def flop(self, deck)-> None:
         assert len(self.board) == 0
         drawn_cards = [deck.draw() for _ in range(3)]
         self.board.extend(drawn_cards)
 
-    def draw_card(self, deck):
+    def draw_card(self, deck)-> None:
         assert len(self.board) <= 5
         self.board.append(deck.draw())
+    
+    def process_decision(self, player):
+        if player.current_decision == "FOLD":
+            print("folding")
+            self.players.pop(self.players.index(player))
+        elif type(player.current_decision) == int:
+            self.pot_size += player.current_decision
+            player.stack -= player.current_decision
+            self.current_bet = player.bet_this_round if player.bet_this_round > self.current_bet else self.current_bet
+    
+    def reset(self):
+        self.pot_size = 0
+        self.current_bet = 0
+        self.players = []
+
+    def end_round(self):
+        assert len(self.players) == 1
+        self.players[0].stack += self.pot_size
+        self.end_round()
