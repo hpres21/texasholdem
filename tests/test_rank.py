e5075c6451c02780296ada1625dc32980cc8d804
jiachenl2021 <jiachenl@princeton.edu>
2022-12-05 20:29:17 -0500

    fixed: test_rank.py

--- tests/test_rank.py
+++ tests/test_rank.py
@@ -3,9 +3,9 @@ import poker_game
 from deck import Card
 import rank
 
-pocket = [Card(value=10, suit='s'), Card(value=10, suit='h')]
-board = [Card(value=10, suit='c'), Card(value=10, suit='d'), Card(value=9, suit='d'), Card(value=8, suit='d'), Card(value=7, suit='d')]
-
-A = rank.BestHand(pocket, board)
-print(A.besthand)
-print(A.rank)
+# pocket = [Card(value=10, suit='s'), Card(value=10, suit='h')]
+# board = [Card(value=10, suit='c'), Card(value=10, suit='d'), Card(value=9, suit='d'), Card(value=8, suit='d'), Card(value=7, suit='d')]
+#
+# A = rank.BestHand(pocket, board)
+# print(A.besthand)
+# print(A.rank)
