6b14ef521570c49fe22e83cc8335a3840a58fb6d
jiachenl2021 <jiachenl@princeton.edu>
2022-12-08 15:13:35 -0500

    format in poker_game.py

--- poker_game.py
+++ poker_game.py
@@ -3,6 +3,7 @@ from deck import Deck, Card
 from rank import BestHand
 from typing import Union
 
+
 @dataclass
 class Player:
     stack: int
@@ -13,20 +14,20 @@ class Player:
     def __repr__(self) -> str:
         return str((self.stack, self.bet_this_round, self.current_decision, self.hand))
 
-    def draw_hand(self, deck: Deck)-> None:
+    def draw_hand(self, deck: Deck) -> None:
         self.hand = [deck.draw() for _ in range(2)]
 
-    def clear_hand(self)-> None:
+    def clear_hand(self) -> None:
         self.hand = []
 
-    def decision(self, current_bet: int)-> None:
+    def decision(self, current_bet: int) -> None:
         """
         Decision method prompts the user to choose an action for their turn.
         """
         action = input(f"You have {self.stack}, the current bet is {current_bet}. Please make a decision: ")
         try:
             action = int(action)
-            if action >= 2*current_bet and action <= self.stack:
+            if action >= 2 * current_bet and action <= self.stack:
                 self.current_decision = action - self.bet_this_round
                 self.bet_this_round += self.current_decision
             else:
@@ -54,8 +55,9 @@ class Player:
                 print("Invalid decision.")
                 self.decision(current_bet)
 
-    def best_hand(self, board: list[Card])-> BestHand:
+    def best_hand(self, board: list[Card]) -> BestHand:
         return BestHand(self.hand, board)
+
 
 @dataclass
 class PokerTable:
@@ -70,32 +72,32 @@ class PokerTable:
     current_bet: int = 0
     deck = Deck()
 
-    def __repr__(self)-> str:
+    def __repr__(self) -> str:
         """
         Not final form of function just for testing right now
         """
         return str((self.pot_size, self.current_bet, self.board, self.players))
 
-    def add_player(self, player: Player)-> None:
+    def add_player(self, player: Player) -> None:
         if len(self.players) <= self.max_num_players:
             self.players.append(player)
         else:
             print("Too many players. Wait for one to leave before you buy in.")
 
-    def remove_player(self, player: Player)-> None:
+    def remove_player(self, player: Player) -> None:
         self.players.pop(self.players.index(player))
         print(f"Player {player} removed from game with ${player.stack}")
 
-    def flop(self)-> None:
+    def flop(self) -> None:
         assert len(self.board) == 0
         drawn_cards = [self.deck.draw() for _ in range(3)]
         self.board.extend(drawn_cards)
 
-    def draw_card(self)-> None:
+    def draw_card(self) -> None:
         assert len(self.board) <= 5
         self.board.append(self.deck.draw())
 
-    def process_decision(self, player: Player)-> None:
+    def process_decision(self, player: Player) -> None:
         """
         Update table based on player's decision. All actions involve a bet >= 0, or a fold.
         """
@@ -107,7 +109,7 @@ class PokerTable:
             player.stack -= player.current_decision
             self.current_bet = player.bet_this_round if player.bet_this_round > self.current_bet else self.current_bet
 
-    def reset(self)-> None:
+    def reset(self) -> None:
         self.pot_size = 0
         self.players = []
         self.board = []
@@ -120,7 +122,6 @@ class PokerTable:
         i = game_hands.index(besthand)
         return self.players[i]
 
-
-    def payout(self, player: Player)-> None:
+    def payout(self, player: Player) -> None:
         player.stack += self.pot_size
-        #self.end_round()
+        # self.end_round()
